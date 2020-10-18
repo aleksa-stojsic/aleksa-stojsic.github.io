5af753873c376fdde74d5aef543865f1183dc863
Aleksa Stojsic <stojsic.aleksa@gmail.com>
2020-10-18 23:04:29 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0150bae52197888994c1.js"
+    "url": "webpack-runtime-7d1ec19081ea271e016c.js"
   },
   {
     "url": "framework-445649399d1c721f1f74.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2e44cb00095b4829cb03041c351d855b"
+    "revision": "798247fbec1aeb1af1b0ab3e14990949"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "d9a7c9d336139f3e0e14233747e82f72"
+    "revision": "b77627797f8c9b45b85fa6e389307751"
   },
   {
     "url": "polyfill-d7c43275abf3c296b887.js"
