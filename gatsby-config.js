dc10727ebe46fb1101f0b0e79e17b914aa66ad39
Aleksa Stojsic <stojsic.aleksa@gmail.com>
2020-12-12 18:25:10 +0100

    PWA tweaks

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,6 +53,9 @@ module.exports = {
         theme_color: colors.lightTheme.primary,
         display: `minimal-ui`,
         icon: siteIcon, // This path is relative to the root of the site.
+        icon_options: {
+          purpose: `any maskable`,
+        },
       },
     },
     {
