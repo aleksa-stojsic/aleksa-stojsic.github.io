1ac12ef306d907f69f083c159fcd53e166adb783
Aleksa Stojsic <stojsic.aleksa@gmail.com>
2020-12-12 18:22:39 +0100

    trying other gtag gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,11 +25,22 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
     {
-      resolve: `gatsby-plugin-gtag`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: `G-HM35T1K4D1`,
-        head: false,
-        anonymize: true,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-HM35T1K4D1", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+        },
+
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+        },
       },
     },
     {
