1fa99d036069a24398fd0c5e01b88bb818c9d0dc
Aleksa Stojsic <stojsic.aleksa@gmail.com>
2021-03-22 17:02:10 +0100

    n3v3n1c4

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -13,7 +13,8 @@ const React = require("react")
 
 exports.onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents([
-    <div className="snow"></div>,
+    // Let it go, let it go...(Frozen)
+    // <div className="snow"></div>,
     <script
       data-name="BMC-Widget"
       src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
